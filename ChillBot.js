68f9ede955a831a112b5d425181938dcbcb97057
ForTakEr <simotonu.prees@tptlive.ee>
2019-06-04 00:06:11 +0300

    Update ChillBot.js

--- ChillBot.js
+++ ChillBot.js
@@ -98,7 +98,7 @@ require('child_process').exec('git rev-parse HEAD', function(err, stdout){
   hash = stdout.slice(0,7)
 })
 var commit;
-require('child_process').exec('git rev-parse "$VAR^{commit}"', function(err, out){
+require('child_process').exec('git rev-parse commit', function(err, out){
   commit = out.toString();
 })
 
@@ -110,7 +110,7 @@ app.post('/git', (req, res) => {
         if (err) console.log(err);
       });
       cmd.run('refresh');
-      console.log(`> [GIT] Updated with origin/master\n` + `      Latest commit: ${commit}`);
+      console.log(`> [GIT] Updated with origin/master\n` + `        Latest commit: ${commit}`);
     }
     return res.sendStatus(200);
   });
